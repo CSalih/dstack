1ad0c07364ab393df45b0251a88a323081b92b21
Salih Candir <salih.candir@styleflasher.at>
2022-05-16 15:35:28 +0200

    feat: set ssh command as default when no invoke command is given

--- dstack/command/__init__.py
+++ dstack/command/__init__.py
@@ -3,16 +3,21 @@ import click
 from dstack.command.docker import ssh, start, stop
 
 
-@click.group()
+@click.group(invoke_without_command=True)
 @click.version_option()
 @click.option('-v', '--verbose', is_flag=True, help='Enables verbose mode', default=False)
 @click.pass_context
 def main(ctx, verbose: bool):
+    """
+    A cli tool to manage your local stack.
+    """
     ctx.obj = {
         "verbose": verbose
     }
 
-    """dstack: wip"""
+    if ctx.invoked_subcommand is None:
+        ctx.invoke(ssh)
+
     pass
 
 
