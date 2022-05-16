a6d6f5a8c475833492c108465004c73ae78cac15
Salih Candir <salih@live.at>
2022-05-16 20:11:28 +0200

    refactor: run linter

--- dstack/command/__init__.py
+++ dstack/command/__init__.py
@@ -6,15 +6,15 @@ from dstack.command.docker import ssh, start, stop
 
 @click.group(invoke_without_command=True)
 @click.version_option()
-@click.option('-v', '--verbose', is_flag=True, help='Enables verbose mode', default=False)
+@click.option(
+    "-v", "--verbose", is_flag=True, help="Enables verbose mode", default=False
+)
 @click.pass_context
 def main(ctx: Context, verbose: bool):
     """
     A cli tool to manage your local stack.
     """
-    ctx.obj = {
-        "verbose": verbose
-    }
+    ctx.obj = {"verbose": verbose}
 
     if ctx.invoked_subcommand is None:
         ctx.invoke(ssh)
