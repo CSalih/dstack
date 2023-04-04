910c208ca5c182398bb9c21dcfd931703c593b92
Salih Candir <salih.candir@styleflasher.at>
2023-04-04 14:45:18 +0200

    ci: bump version to 0.2.0-alpha.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="dstack",
-    version="0.1.1",
+    version="0.2.0-alpha.0",
     description="A cli tool to manage your local stack",
     author="CSalih (Salih Candir)",
     url="https://github.com/CSalih/dstack",
